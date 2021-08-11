6894419ca6d5af273a64eaa4e0499ea66d97bcd2
hello <abenezermeheret76@gmail.com>
2021-08-11 11:35:02 +0300

    Aded DataPreprocessing, InputPipeline, Model files

---
+++ Model.py
@@ -0,0 +1,19 @@
+import ktrain
+from ktrain import text
+
+# input data
+docs = []
+
+
+# create a search index
+INDEXDIR = '/tmp/myindex'
+text.SimpleQA.initialize_index(INDEXDIR)
+text.SimpleQA.index_from_list(docs,INDEXDIR,commit_every=len(docs))
+
+# create QA instance
+qa = text.SimpleQA(INDEXDIR)
+
+# asking questions
+question = ""
+answers = qa.ask('')
+answers = answers[:5] # return the first five answers
--- api.py
+++ api.py
@@ -1,2 +1,12 @@
 # this file contains the api wrapper that are used
 # receive and send requests to and from the server.
+import flask
+from flask import request,jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# define the API route here
+@app.route('/',methods=['GET'])
+def home():
+    return '''Something is fishy here'''
